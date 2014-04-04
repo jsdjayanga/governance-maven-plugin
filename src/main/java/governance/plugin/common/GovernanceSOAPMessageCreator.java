030b74edd58d320dfe41b9cbd2141e65310ea457
Jayanga Dissanayake <jsdjayanga@yahoo.com>
2014-04-04 17:11:29 +0530

    Updated to add dependent osgi services.

--- src/main/java/governance/plugin/common/GovernanceSOAPMessageCreator.java
+++ src/main/java/governance/plugin/common/GovernanceSOAPMessageCreator.java
@@ -242,6 +242,35 @@ public class GovernanceSOAPMessageCreator {
         soapRequest.append("</soapenv:Envelope>");
         return soapRequest.toString();
     }
+
+    public static String  createAddOSGiServiceRequest(String name, String namespace, String version, String description){
+        StringBuffer soapRequest = new StringBuffer();
+        soapRequest.append("<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' ");
+        soapRequest.append("xmlns:ser='http://services.add.osgiservice.governance.carbon.wso2.org'>");
+        soapRequest.append("<soapenv:Header/>");
+        soapRequest.append("<soapenv:Body>");
+        soapRequest.append("<ser:addOSGiService>");
+        soapRequest.append("<ser:info><![CDATA[<metadata xmlns='http://www.wso2.org/governance/metadata'><overview><name>");
+        soapRequest.append(name);
+        soapRequest.append("</name>");
+        soapRequest.append("<namespace>");
+        soapRequest.append(namespace);
+        soapRequest.append("</namespace>");
+        soapRequest.append("<version>");
+        soapRequest.append(version);
+        soapRequest.append("</version>");
+        soapRequest.append("<description>");
+        soapRequest.append(description);
+        soapRequest.append("</description>");
+        soapRequest.append(GovernanceSOAPMessageCreator.getMetaDataTags());
+        soapRequest.append("</overview>");
+        soapRequest.append(GovernanceSOAPMessageCreator.getImageTags());
+        soapRequest.append("</metadata>]]></ser:info>");
+        soapRequest.append("</ser:addOSGiService>");
+        soapRequest.append("</soapenv:Body>");
+        soapRequest.append("</soapenv:Envelope>");
+        return soapRequest.toString();
+    }
     
     public static String createEditArtifactMessage(String resourcePath, String assetType, String data){
     	StringBuffer soapRequest = new StringBuffer();
